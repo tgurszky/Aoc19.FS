e2d489d6e84d77791e6c68ac90ab68929d7e5500
Tamás Gurszky <tamas.gurszky@gmail.com>
2020-10-17 22:20:21 +0200

    PascalCase properties, switch to discriminated union from enum type

--- Aoc19/IntcodeComputer.fs
+++ Aoc19/IntcodeComputer.fs
@@ -1,44 +1,44 @@
 ﻿module IntcodeComputer
 
-type OpCode = Add = 1 | Multiply = 2 | Halt = 99
+type OpCode = Add | Multiply | Halt
 type Program = int array
 type ExecutionContext = {
-    currentOp: OpCode option
-    operandA: int option
-    operandB: int option
-    state: Program
+    CurrentOp: OpCode option
+    OperandA: int option
+    OperandB: int option
+    State: Program
 }
 
 let identifyOperation item =
     match item with
-    | 1 -> Some OpCode.Add
-    | 2 -> Some OpCode.Multiply
-    | 99 -> Some OpCode.Halt
+    | 1 -> Some Add
+    | 2 -> Some Multiply
+    | 99 -> Some Halt
     | _ -> None
 
 let update index value state =
     Array.mapi (fun i v -> if i = index then value else v) state
 
 let handleTwoOperands operation context current =
-    match context.operandA with
-    | None -> { context with operandA = Some (Array.item current context.state) }
-    | Some opAVal -> match context.operandB with
-                        | None -> { context with operandB = Some (Array.item current context.state) }
+    match context.OperandA with
+    | None -> { context with OperandA = Some (Array.item current context.State) }
+    | Some opAVal -> match context.OperandB with
+                        | None -> { context with OperandB = Some (Array.item current context.State) }
                         | Some opBVal -> { 
-                            currentOp = None; 
-                            operandA = None; 
-                            operandB = None; 
-                            state = update current (operation opAVal opBVal) context.state }
+                            CurrentOp = None; 
+                            OperandA = None; 
+                            OperandB = None; 
+                            State = update current (operation opAVal opBVal) context.State }
 
 
 let executeStep context index =
-    match context.currentOp with
-        | None -> { context with currentOp = identifyOperation (Array.item index context.state) }
-        | Some OpCode.Add -> handleTwoOperands (+) context (Array.item index context.state)
-        | Some OpCode.Multiply -> handleTwoOperands (*) context (Array.item index context.state)
+    match context.CurrentOp with
+        | None -> { context with CurrentOp = identifyOperation (Array.item index context.State) }
+        | Some Add -> handleTwoOperands (+) context (Array.item index context.State)
+        | Some Multiply -> handleTwoOperands (*) context (Array.item index context.State)
         | Some _ -> context
 
 let run program =
-    let initialContext = { currentOp = None; operandA = None; operandB = None; state = program }
-    let { state = result } = [ 0 .. program.Length - 1] |> List.fold executeStep initialContext
+    let initialContext = { CurrentOp = None; OperandA = None; OperandB = None; State = program }
+    let { State = result } = [ 0 .. program.Length - 1] |> List.fold executeStep initialContext
     result
